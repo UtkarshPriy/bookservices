decf3202ede50a87e2dc71f768ad5b9ca1e4b7a2
UtkarshPriy <100868987+UtkarshPriy@users.noreply.github.com>
2026-01-20 12:33:00 +0530

    mockDB added

--- booksApp/src/handlers/  updateBook:.ts
+++
@@ -1,7 +0,0 @@
-export async function updateBook(event: any) {
-  console.log("Book Updated");
-  return {
-    statusCode: 201,
-    body: JSON.stringify({ message: "Book Updated!" }),
-  };
-}
--- booksApp/src/handlers/createBook.ts
+++ booksApp/src/handlers/createBook.ts
@@ -1,7 +1,44 @@
+import z, { string } from "zod";
+import { BookModel, createBookmodel } from "../models/BookModel.js";
+
+const bookSchema = z.object({
+  title: z.string(),
+});
+
+export type bookBodytype = z.infer<typeof bookSchema>;
+
+class CreateBookHandler {
+  constructor(private readonly bookModel: BookModel) {}
+
+  async processEvent(event: any) {
+    console.log("Book Created");
+
+    // Validation of body
+    const body = JSON.parse(event.body);
+    const safeBody = bookSchema.parse(body);
+    // BookModel(safeBody);
+    const book = await this.bookModel.createBook(safeBody);
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({ data: book }),
+    };
+  }
+}
+
 export async function createBook(event: any) {
-  console.log("Book Created");
-  return {
-    statusCode: 201,
-    body: JSON.stringify({ message: "Book Created!" }),
-  };
+  try {
+    const bookModelInstance = createBookmodel();
+    const instance = new CreateBookHandler(bookModelInstance);
+
+    return await instance.processEvent(event);
+  } catch (error) {
+    console.error("Error", error);
+    return {
+      statusCode: 501,
+      body: JSON.stringify({
+        msg: "Some error happened while creating the book",
+      }),
+    };
+  }
 }
